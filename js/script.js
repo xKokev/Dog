4182d736bb028d3bd9bfa86915f91b2d9d1193c8
Kevin <10kevinho@gmail.com>
2021-06-21 15:47:07 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const $portfolio = document.querySelector('.sec-portfolio-js');
+const $modalImgPortfolio = document.querySelector('.img-modal-js');
+$portfolio.addEventListener('click', (e) => {
+  if (e.target.classList.contains('img-btn-modal-js')) {
+    let urlImg = e.target.attributes[0].nodeValue;
+    $modalImgPortfolio.src = urlImg;
+  }
+});
